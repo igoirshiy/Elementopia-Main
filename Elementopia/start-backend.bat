a9d7861f79325e93c6dba33d2bba88dbaab214d3
igoirshiy <wayneskiy@gmail.com>
2026-09-16 23:12:07 +0800

    fix: configure PostgreSQL dialect and JDK fallback for backend

--- Elementopia/start-backend.bat
+++ Elementopia/start-backend.bat
@@ -1,5 +1,9 @@
 @echo off
 echo Starting Elementopia Backend...
-set "JAVA_HOME=C:\Program Files\Java\jdk-21"
+if exist "C:\Program Files\Java\jdk-22" (
+    set "JAVA_HOME=C:\Program Files\Java\jdk-22"
+) else if exist "C:\Program Files\Java\jdk-21" (
+    set "JAVA_HOME=C:\Program Files\Java\jdk-21"
+)
 cd /d "%~dp0BACKEND"
 call mvnw.cmd spring-boot:run
